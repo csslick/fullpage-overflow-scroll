a3e511072b225efafad14a7077cce41559247e78
csslick <digitone@naver.com>
2022-09-05 23:14:41 +0900

    translate check
    스크롤 섹션 translateY의 스크롤 이동값 판정

--- script.js
+++ script.js
@@ -15,14 +15,35 @@ $(document).ready(function () {
     // 페이지 진입시(액션)
     afterLoad: function (anchorLink, index) {
       console.log(index);
+
       // 4번째 페이지에 들어오면
       if(index == 4) {
         $('#section4 > div').append('<p>원대한 꿈에 도약합니다!</p>');
         $('#rocket').addClass('play');
       } else { $('#rocket').removeClass('play'); }
-    }
+    },
+
   });
+
+  $('#section3').on('mousewheel DOMMouseScroll drag', function (e) {
+  
+    let el = document.querySelectorAll('#section3 .fp-scroller')[0];
+    let { translateY } = getTranslateXY(el)  
+    console.log(translateY)
+
+    if(translateY < -200) {
+      $(this).find('.box').eq(1).css('color', 'red')
+    }
+});
 
 });
 
-
+// translateXY 이동값 채크
+function getTranslateXY(element) {
+  const style = window.getComputedStyle(element)
+  const matrix = new DOMMatrixReadOnly(style.transform)
+  return {
+      translateX: matrix.m41,
+      translateY: matrix.m42
+  }
+}
